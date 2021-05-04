cc1b788525c9b31676148db327c6fd5d75bbfa20
angelsumalini <70747259+angelsumalini@users.noreply.github.com>
2021-05-04 11:52:37 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,20 @@
+class student
+{
+int id;
+String name;
+student()
+{
+}
+void display()
+{
+System.out.println(id+" "+name);
+}
+}
+class student1
+{
+public static void main(String args[])
+{
+student s1=new student();
+student s2=new student();
+}
+}
